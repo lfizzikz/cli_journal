2801ae831e25399a4053031a5b52b04bbc6679b5
Trevor Nance <trevornance7@gmail.com>
2025-12-17 18:45:32 -0400

    fixed format and added timestamp

--- main.go
+++ main.go
@@ -1,34 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"time"
 )
 
 type FileInfo struct {
-	content string
-	title   string
+	content   string
+	title     string
+	fullPath  string
+	entryTime string
 }
 
 var VaultPath = "/Users/trevornance/Documents/My Vault/Daily Writing/"
 
 func main() {
+
 	extension := ".md"
 	entry := os.Args[1:]
-	currentDate := time.Now()
-	formattedDate := currentDate.Format("2006-01-02")
+	currentDateTime := time.Now()
+	formattedDate := currentDateTime.Format("2006-01-02")
+	formattedTime := currentDateTime.Format("15:04")
 	entryToSave := strings.Join(entry, " ")
 	fullPath := VaultPath + formattedDate + extension
+
 	newFile := FileInfo{
-		content: entryToSave,
-		title:   fullPath,
+		content:   entryToSave,
+		title:     formattedDate,
+		fullPath:  fullPath,
+		entryTime: formattedTime,
 	}
-	file, err := os.Create(newFile.title)
+
+	writeToFile(newFile)
+}
+
+func writeToFile(f FileInfo) {
+	file, err := os.OpenFile(
+		f.fullPath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error: %s\nFile: %s", err, f.title)
 	}
 	defer file.Close()
 
-	file.WriteString(newFile.content)
+	file.WriteString("-[" + f.entryTime + "] " + f.content + "\n\n")
 }
